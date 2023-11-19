15c3bc2e2700d60c05afef0e62884f102c77cd35
MYSTICLWN <hendraoktobiananfasa678@gmail.com>
2023-11-20 06:28:42 +0800

    Portfolio with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#8b5cf6",
+        secondary: "#020617",
+      },
+    },
+  },
+  plugins: [],
+};
